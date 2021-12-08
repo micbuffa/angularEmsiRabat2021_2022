01f4841bd3b696d74920f367237a1131a52baf10
micbuffa <micbuffa@gmail.com>
2021-12-08 17:19:35 +0100

    exemple formulaire ajout d'assignment et binding bi directionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatDividerModule } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { RenduDirective } from './shared/rendu.directive';
 import { NonRenduDirective } from './shared/non-rendu.directive';
@@ -19,8 +24,9 @@ import { NonRenduDirective } from './shared/non-rendu.directive';
     NonRenduDirective
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule, MatButtonModule, MatIconModule, MatDividerModule
+    BrowserModule, FormsModule,
+    BrowserAnimationsModule, MatButtonModule, MatIconModule, MatDividerModule,
+    MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Assignment } from './assignment.model';
 
 @Component({
   selector: 'app-assignments',
@@ -7,33 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentsComponent implements OnInit {
   titre = "Liste des assignments";
-  couleur = "violet"
-  assignments = [
+  couleur = "violet";
+  // pour le formulaire
+  nomAssignment:string = "";
+  dateDeRendu?:Date = undefined;
+
+  assignments:Assignment[] = [
     {
       nom:"Devoir Angular proposé par Mr Buffa",
-      dateDeRendu: "2022-01-23",
+      dateDeRendu: new Date("2022-01-23"),
       rendu : false
     },
     {
       nom:"Devoir gestion de projet de Mr Winter",
-      dateDeRendu: "2022-01-10",
+      dateDeRendu: new Date("2022-01-10"),
       rendu : false
     },
     {
       nom:"Devoir Hybride de Mr Pascal Bohn",
-      dateDeRendu: "2021-11-01",
+      dateDeRendu: new Date("2021-11-01"),
       rendu : true
     }
   ]
   constructor() { }
 
   ngOnInit(): void {
+    // appelé AVANT l'affichage (juste après le constructeur)
+    console.log("AVANT AFFICHAGE");
   }
 
-  getColor(i:number) {
-    if(i===0) return 'blue';
-    if (i===1) return 'green';
+  onSubmit() {
+    console.log(this.dateDeRendu);
 
-    return "orange"
+    if(this.nomAssignment && this.dateDeRendu) {
+      let nouvelAssignment = new Assignment();
+
+      nouvelAssignment.nom = this.nomAssignment;
+      nouvelAssignment.dateDeRendu = this.dateDeRendu;
+      nouvelAssignment.rendu = false;
+
+      this.assignments.push(nouvelAssignment);
+    }
+
   }
 }
